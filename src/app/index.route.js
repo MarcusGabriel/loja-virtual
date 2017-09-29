8612fae1060fa4aa65ee6d1adcd2aba411970929
Marcus Gabriel Soares <marcus.gbrl@live.com>
2017-09-28 23:00:56 -0400

    versão final do projeto

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,21 +8,21 @@
   function routeConfig($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-      .when('/', {
-        templateUrl: 'app/produto/produtos.html',
-        controller: 'ProdutosController'
-      })
-      .when('/produto/editar/:produtoId',{
-        templateUrl : 'app/produto/produto.html',
-        controller : 'ProdutoController'
-      })
-      .when('/produto/novo',{
-        templateUrl : 'app/produto/produto.html',
-        controller : 'ProdutoController'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    .when('/', {
+      templateUrl: 'app/produto/produtos.html',
+      controller: 'ProdutosController',
+    })
+    .when('/produto/editar/:produtoId',{
+      templateUrl : 'app/produto/produto.html',
+      controller : 'ProdutoController',
+    })
+    .when('/produto/novo',{
+      templateUrl : 'app/produto/produto.html',
+      controller : 'ProdutoController',
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
   }
 
 })();
--- src/app/produto/produto-controller.js
+++ src/app/produto/produto-controller.js
@@ -6,30 +6,41 @@
       .controller('ProdutoController', ProdutoController);
   
     /** @ngInject */
-    function ProdutoController($scope, $http, $routeParams, toastr) {
+    function ProdutoController($scope, $http, $routeParams, toastr, $timeout, $location) {
         if($routeParams.produtoId){
             $http.get('http://localhost:8080/produto/'+ $routeParams.produtoId)
             .success(function(produto){
                 $scope.produto = produto[0];
             })
             .error(function(){
-                console.log('produto não encontrada');
-                $scope.mensagem = 'id não encontrado';
+                toastr.error("Produto não encontrado");
+                $timeout(function() {
+                    $location.path('/');
+                }, 1500);
             });
         }
 
         $scope.produto = {};
-        $scope.mensagem = '';
 
-        
+        $scope.filterValue = function($event){
+            if(isNaN(String.fromCharCode($event.keyCode))){
+                $event.preventDefault();
+            }
+        };
         $scope.submeter = (function (){
             if($scope.produto._id){
                 $http.put('http://localhost:8080/produto/'+$scope.produto._id, $scope.produto)
                 .success( function(){
                     toastr.success("Produto atualizado com sucesso!");
+                    $timeout(function() {
+                        $location.path('/');
+                    }, 1500);
                 })
                 .error(function(){
                     toastr.error("Ocorreu um erro! Tente novamente.", "Error");
+                    $timeout(function() {
+                        $location.path('/');
+                    }, 1500);
                 })
             }else{ 
                 if($scope.formulario.$valid){
@@ -37,15 +48,32 @@
                     .success( function() {
                         $scope.produto = {};
                         toastr.success("Produto cadastrado com sucesso!");
+                        $timeout(function() {
+                            $location.path('/');
+                        }, 1500);
                     })
                     .error(function (error)  {
-                        $scope.mensagem = 'produto não cadastrada com sucesso';
-                        console.log(error);
+                        toastr.error("Ocorreu um erro! Tente novamente.", "Error");
+                        $timeout(function() {
+                            $location.path('/');
+                        }, 1500);
                     });
                 }
             }
         });
-      
+        
+        $scope.fotoAleatoria = function (produto) {
+            var nome = produto.nome;
+            $http.get('https://api.gettyimages.com/v3/search/images?phrase='+nome, {
+                headers: {'Api-Key': 'bpaup4cjrzzddx6zgkmjhbc9'}
+            })
+            .success(function(retorno){
+                $scope.produto.imagem = retorno.images[0].display_sizes[0].uri;
+            })
+            .error(function(error){
+                console.log(error);  
+            });
+        }
     }
   })();
   
